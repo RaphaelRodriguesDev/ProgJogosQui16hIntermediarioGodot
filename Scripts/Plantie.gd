7b429bf57f7c72c75eae4c0e86afc92742932b6c
RaphaelRodriguesDev <professor.dev.raphael@gmail.com>
2025-07-17 17:51:22 -0300

    Trabalhando com a Bullet

--- Scripts/Plantie.gd
+++ Scripts/Plantie.gd
@@ -5,17 +5,30 @@ var facing_left = true
 onready var bullet_instance = preload("res://Scene/seed.tscn")
 onready var player = Global.get("player")
 
-func _physics_process(delta) -> void:
+func _physics_process(_delta) -> void:
 	if player:
 		var distance = player.global_position.x - self.position.x
 		facing_left = true if distance < 0 else false
-		
-		if facing_left:
-			self.scale.x = 1
-		else:
-			self.scale.x = -1
+	
+	if facing_left:
+		self.scale.x = 1
+	else:
+		self.scale.x = -1
 			
 func shoot():
 	var bullet = bullet_instance.instance()
-	add_child(bullet)
+	get_parent().add_child(bullet)
 	bullet.global_position = $spawnShoot.global_position
+	
+
+
+func _on_playerDetector_body_entered(body):
+	$anim.play("attack")
+
+
+func _on_playerDetector_body_exited(body: Node) -> void:
+	$anim.play("idle")
+
+
+func _on_hitbox_body_entered(body: Node) -> void:
+	queue_free()
