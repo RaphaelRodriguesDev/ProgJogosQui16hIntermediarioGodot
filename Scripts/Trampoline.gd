fabc5e442d103454c8cd566f657353de70c92777
RaphaelRodriguesDev <professor.dev.raphael@gmail.com>
2025-08-21 16:18:56 -0300

    Trampolim

--- Scripts/Trampoline.gd
+++ Scripts/Trampoline.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 func _on_trampoline_body_entered(body: Node) -> void:
 	print("Colidiu")
-	body.velocity.y = body.jump_force
+	body.velocity.y = body.jump_force * 2
 	$anim.play("jump")
